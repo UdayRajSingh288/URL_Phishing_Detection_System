2ba6809d375cf29fcbef24959afc16a2acda91b2
udayrajsingh288 <udayrajsingh288@gmail.com>
2025-04-18 09:11:11 +0530

    webapp progress chkpt 4

--- url_phishing_detection_system/user/urls.py
+++ url_phishing_detection_system/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home_view, signup_view, signin_view, detect_view
+from .views import home_view, signup_view, signin_view, detect_view, logout_view, delete_view, testURL_view
 
 app_name  = 'user'
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
 	path('signup/', signup_view, name = 'signup'),
 	path('signin/', signin_view, name = 'signin'),
 	path('detect/', detect_view, name = 'detect'),
+	path('logout/', logout_view, name = 'logout'),
+	path('delete/', delete_view, name = 'delete'),
+	path('testURL/', testURL_view, name = 'testURL'),
 ]
--- url_phishing_detection_system/user/views.py
+++ url_phishing_detection_system/user/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import User
 from bcrypt import hashpw, gensalt, checkpw
+from requests import post
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def check_login(request):
 	e = request.COOKIES.get('email')
@@ -45,7 +49,6 @@ def signin_view(request):
 		if len(User.objects.filter(email = e)) == 1:
 			u = User.objects.get(email = e)
 			if checkpw(p.encode(encoding = 'utf-8'), u.password):
-				print('User matched!')
 				response = redirect('user:detect')
 				response.set_cookie('email', e)
 				response.set_cookie('password', p)
@@ -59,4 +62,44 @@ def signin_view(request):
 def detect_view(request):
 	if check_login(request) == False:
 		return redirect('user:home')
+	if request.POST:
+		url = request.POST['url']
+		resp = post('http://127.0.0.1:8080', json = {'url': url})
+		return render(request, 'user/message.html', {'msg': resp.text})
 	return render(request, 'user/detect.html', {})
+
+def logout_view(request):
+	if check_login(request) == False:
+		return redirect('user:home')
+	else:
+		response = redirect('user:home')
+		response.delete_cookie('email')
+		response.delete_cookie('password')
+		return response
+
+def delete_view(request):
+	if check_login(request) == False:
+		return redirect('user:home')
+	else:
+		e = request.COOKIES['email']
+		u = User.objects.get(email = e)
+		u.delete()
+		response = redirect('user:home')
+		response.delete_cookie('email')
+		response.delete_cookie('password')
+		return response
+
+@api_view(['POST'])
+def testURL_view(request):
+	e = request.data.get('email')
+	p = request.data.get('password')
+	l = request.data.get('url')
+	if len(User.objects.filter(email = e)) == 1:
+		u = User.objects.get(email = e)
+		if checkpw(p.encode(encoding = 'utf-8'), u.password):
+			resp = post('http://127.0.0.1:8080', json = {'url': l})
+			return Response({'status': resp.text}, status=status.HTTP_200_OK)
+		else:
+			return Response({'status': 'PASSWORD INCORRECT!'}, status=status.HTTP_200_OK)
+	else:
+		Response({'status': 'USER NOT FOUND!'}, status=status.HTTP_200_OK)
