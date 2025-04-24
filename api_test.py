4c5dc10c98b6da98dfb85011eadee1c1e275bbab
udayrajsingh288 <udayrajsingh288@gmail.com>
2025-04-24 10:35:28 +0530

    final year project completed and final presentation ready

--- api_test.py
+++ api_test.py
@@ -1,9 +1,9 @@
 from requests import post
 
 d = {
-	'email': 'udarajsingh288@gmail.com',
+	'email': 'udayrajsingh288@gmail.com',
 	'password': 'Qwer1234%',
-	'url': 'https://google.com'
+	'url': 'https://youtube.com'
 }
 
 r = post('http://127.0.0.1:8000/testURL/', json = d)
